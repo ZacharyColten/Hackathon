4ef85fb535b51aa6cddc9135c710f8b5d93311de
Zachary Colten <zacharycolten@gmail.com>
2020-03-02 09:28:00 -0600

    hello

---
+++ hackathon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewsFeed from './components/NewsFeed';
+
+function App() {
+  return (
+    <div>
+      <NewsFeed />
+    </div>
+  );
+}
+
+export default App;
