b56d074ed7849defccb42bbc50d91bb3ac10d4fa
zacharycolten <zacharycolten@gmail.com>
2020-03-04 12:44:36 -0600

    turn-in ready

--- hackathon/src/components/NewsFeed.js
+++ hackathon/src/components/NewsFeed.js
@@ -2,42 +2,57 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem'
 
 class NewsFeed extends Component {
-
-  state = {
+  constructor(props) {
+    super(props);
+  this.state = {
     items: [],
     value: ''
+    
   }
-
-  fetchItems = (e) => {
-    fetch(`https://hn.algolia.com/api/v1/search_by_date?query=...
-    `)
+  
+  this.onChange = this.onChange.bind(this)
+}
+  
+  fetchItems = () => {
+    this.state.value == '' ? window.alert("Type in the search bar and click submit to search articles") : 
+    fetch(`http://hn.algolia.com/api/v1/search?query=${this.state.value}`)
       .then(results => results.json())
       .then(data => {
         this.setState({
-          value: e.target.value,
-          items: data
+          items: data.hits,
+          value: ''
         })
-
-
       })
-
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({value: e.target.value})
+    this.fetchItems()
+    
+  }
+  onChange(e){
+    this.setState({value : e.target.value});
+}
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          {/* they value of the input is tied to this.state.value so when a user types the handleChange method changes this.state.value to match*/}
-          <input type="text" value={this.state.value} onChange={this.fetchItems} />
-        </label>
+<form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.onChange}/>
         <input type="submit" value="Submit" />
-      </form>
-        {
+        </form>
+    <p style={{'background': '#f4f4f4',
+      'padding': '10px',
+      'borderBottom': '1px #ccc dotted', 'font-weight':'bold'}}>Algolia Hacker News Search</p>
+      {this.state.items.length === 0 ? (
+        <p>Type in search bar and click submit to search articles</p>
+      ) : (
+        
           this.state.items.map(item => {
-            return <NewsItem name={item.story_title} date={item.created_at}/>
+            return <NewsItem  name={item.author} date={item.created_at} title={item.title} url={item.url}/>
           })
-        }
+        
+      )}
+      
       </div>
     );
   }
--- hackathon/src/components/NewsItem.js
+++ hackathon/src/components/NewsItem.js
@@ -4,8 +4,16 @@ class NewsItem extends Component {
 
   render() {
     return(
-      <div>
-    Title: {this.props.name} Date: {this.props.date}
+      <div style={{'background': '#f4f4f4',
+      'padding': '10px',
+      'borderBottom': '1px #ccc dotted'}}>
+    <b>Author:</b> {this.props.name} 
+    <br></br>
+    <b>Date:</b> {this.props.date} 
+    <br></br>
+    <b>Title:</b> {this.props.title}
+    <br></br>
+    <a href={this.props.url} target="_blank">Click here to open this article in a new tab</a>
     </div>
     )
     }
